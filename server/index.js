8954fea57d833d8b615d5f906a8128f711f5cc13
zz7 <iltvxqsj@163.com>
2019-05-26 22:41:37 +0800

    wechat login

--- server/index.js
+++ server/index.js
@@ -22,8 +22,21 @@ app.get('/search', (req, res) => {
 	res.send("search!");
 });
 
+//==========================WECHAT================================
+
+
 var OAuth = require('wechat-oauth');
 var oauthApi = new OAuth('wxed3140d08c8341d5','1a4a7ed9206f4f26a6f7597c357c9651');
+var red_url = 'https://u-know-what.herokuapp.com/gettoken';
+
+app.get('/wechat', function(req, res) {
+	var url = oauthApi.getAuthorizeURL(red_url,'','snsapi_base');
+	console.log(url);   
+	res.redirect(url); 
+});
+
+
+
 app.get('/callback', function (req, res) {  
 	console.log('----weixin callback -----');
 	var code = req.query.code;
@@ -44,21 +57,71 @@ app.get('/callback', function (req, res) {
 	});
 });
 
-var callbackURL = 'https://u-know-what.herokuapp.com/callback';
-
-app.get('/oauth2', function(req, res) {
-	var url = oauthApi.getAuthorizeURL(callbackURL,'','snsapi_base');
-	console.log(url);   
-//	res.redirect(url); 
-});
-
 
 app.post('/gettoken', (req, res) => {
 	console.log("home post.");
 	res.send("Hello post!");
 })
 
+// ==========================GITHUB================================
+/**
+app.get('/login', checkNotLogin, async (req, res, next) => {
+    const dataStr = (new Date()).valueOf()
+    //  重定向到认证接口,并配置参数
+    let path = "https://github.com/login/oauth/authorize"
+    path += '?client_id=' + config.client_id
+    path += '&scope=' + config.scope
+    path += '&state=' + dataStr
+    // 转发到授权服务器
+    res.redirect(path)
+})
 
+app.get('/oauth/callback', checkNotLogin, (req, res, next) => {
+    const code = req.query.code;
+    let path = 'https://github.com/login/oauth/access_token';
+    const params = {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        code: code
+    }
+    fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    })
+    .then(result => {
+        return result.text()
+    }).then(body => {
+        let access_token = body.split('&')[0].split('=')[1]
+        return access_token
+    }).then(token => {
+        const url = ' https://api.github.com/user?access_token=' + token;
+        fetch(url)
+            .then(info => {
+                return info.json();
+            })
+            .then(github_info => {
+                UserModel.getUserByOauthInfo({ type: 'github', name: github_info.login }).then(user => {
+                    if (user) {
+                        // 已注册，获取登录信息后直接跳转到列表页
+                        user = user.toObject()
+                        delete user.password
+                        req.session.user = JSON.parse(JSON.stringify(user))
+                        res.redirect(`${config.main_url}?username=${user.username}`)
+                    } else {
+                        // 如果没有注册，就跳转到注册界面
+                        res.redirect(`${config.register_url}?name=${github_info.login}&type=github&avatar_url=${github_info.avatar_url}&bio=${github_info.bio}`)
+                    }
+                })
+                
+            })
+
+    })
+})
+
+**/
 
 PORT = process.env.PORT || 5000
 app.listen(PORT);

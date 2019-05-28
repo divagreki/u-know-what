22dd9ddb04c64d0354d09577701a10c90ddeb40c
Alison <AlphaUMa@users.noreply.github.com>
2019-05-28 12:39:56 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -147,98 +147,24 @@ app.get('/github', function(req, res) {
     res.redirect(path)
 });
 
-/**
+var request = require('request');
+
 app.get('/github_callback', function(req, res) {
-    const code = req.query.code;
-    let path = 'https://github.com/login/oauth/access_token';
-
-    const params = {
-        client_id: githubConfig.client_id,
-        client_secret: githubConfig.client_Secret,
-        code: code
-    }
-    fetch(path, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(params)
-    })
-    .then(result => {
-        return result.text()
-    }).then(body => {
-        let access_token = body.split('&')[0].split('=')[1]
-        return access_token
-    }).then(token => {
-        const url = ' https://api.github.com/user?access_token=' + token;
-        res.send('token = '+token);
-        fetch(url)
-            .then(info => {
-                return info.json();
-            })
-            .then(github_info => {
-                res.send('github_info: '+ github_info);
-                // UserModel.getUserByOauthInfo({ type: 'github', name: github_info.login }).then(user => {
-                //     if (user) {
-                //         // 已注册，获取登录信息后直接跳转到列表页
-                //         user = user.toObject()
-                //         delete user.password
-                //         req.session.user = JSON.parse(JSON.stringify(user))
-                //         res.redirect(`${config.main_url}?username=${user.username}`)
-                //     } else {
-                //         // 如果没有注册，就跳转到注册界面
-                //         res.redirect(`${config.register_url}?name=${github_info.login}&type=github&avatar_url=${github_info.avatar_url}&bio=${github_info.bio}`)
-                //     }
-                // })
-                
-            })
-
-    })
-    
-});
-
-**/
-
-app.get("/github_callback", function(req, res){
     var code = req.query.code;
-    var state = req.query.state;
-    var headers = req.headers;
-    var path = "/login/oauth/access_token";
-    headers.host = 'github.com';
-
-    path += '?client_id=' + githubConfig.client_ID;
-    path += '&client_secret='+ githubConfig.client_Secret;
-    path += '&code='+ code;
-    console.log(path);
-    var opts = {
-        hostname:'github.com',
-        port:'443',
-        path:path,
-        headers:headers,
-        method:'POST'
+    var myJSONObject = {
+	"client_id" : githubConfig.client_ID, 
+	"client_secret" : githubConfig.client_Secret,
+	"code" : code
     };
-
-    var req = https.request(opts, function(res){
-        res.setEncoding('utf8');
-         console.log(opts);
-    
-        res.on('data', function(data){
-            var args = data.split('&');
-            var tokenInfo = args[0].split("=");
-            var token = tokenInfo[1];
-            res.send('data = '+ data +' token = '+token);
-            console.log(data);
-
-            // var url = "https://api.github.com/user?access_token="+token+"&scope=user";
-            // https.get(url, function(res){
-            //     res.on('data', function(userInfo){
-            //        console.log(userInfo);
-            //        res.send('userInfo: '+ userInfo)
-            //     });
-            // });
-          
-        })
-    });
+	request({
+	    url: "https://github.com/login/oauth/access_token",
+	    method: "POST",
+	    json: true,   // <--Very important!!!
+	    body: myJSONObject
+	}, function (error, response, body){
+	    console.log(response);
+	});
+	
 });
 
 

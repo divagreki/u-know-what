9b72ecaf826934718f978d8448435ac2b9aba388
Alison <AlphaUMa@users.noreply.github.com>
2019-05-28 14:46:41 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -78,6 +78,14 @@ app.get('/search', (req, res) => {
 	console.log("get:search");
 	res.send("search!");
 });
+app.get('/ppp', (req, res) => {
+	var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+});
 
 //==========================WECHAT================================
 

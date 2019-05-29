1a053ccf785138631a48b1f95abd83639490dd4c
xzhan103 <diamu1992@gmail.com>
2019-05-29 16:46:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: '/auth/google/callback',
-			proxy: true
+			callbackURL: 'https://u-know-what.herokuapp.com/auth/google/callback'
 		}, 
 		(accessToken, refreshToken, profile, done) =>	{
 			console.log(profile);

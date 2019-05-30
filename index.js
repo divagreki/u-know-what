25bfc1a8512d3bdce659e77f14bb32a4114dceed
Alison <AlphaUMa@users.noreply.github.com>
2019-05-30 14:29:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const session = require('express-session');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-var request = require('request');
-var needle = require('needle');
-
 
 
 passport.serializeUser(function(user, done) {
